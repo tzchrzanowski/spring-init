6ece78b9b0fea03e9cfb344c25896daa51fb8471
tzchrzanowski <tzchrzanowski@gmail.com>
2023-09-24 08:54:55 +0800

    added bean with custom name, and basic data types beans

--- src/main/java/config/ProjectConfig.java
+++ src/main/java/config/ProjectConfig.java
@@ -7,8 +7,24 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
     @Bean
-    Parrot parrot() {
+    Parrot parrotOne() {
         var rio = new Parrot("Rio");
         return rio;
+    }
+
+    @Bean(name="parotte")
+    Parrot parrotTwo() {
+        var parroterinio = new Parrot("Parroterinio");
+        return parroterinio;
+    }
+
+    @Bean
+    String itsOverCaption() {
+        return "Its over..";
+    }
+
+    @Bean
+    Integer nineK() {
+        return 9000;
     }
 }
--- src/main/java/main/Main.java
+++ src/main/java/main/Main.java
@@ -16,7 +16,20 @@ public class Main {
         /*
         * Initializing new parrot object using bean:
         * */
-        Parrot rioBean = context.getBean(Parrot.class);
+        Parrot rioBean = context.getBean("parrotOne", Parrot.class);
         System.out.println(rioBean.getName());
+
+        Parrot parroterinioBean = context.getBean("parotte", Parrot.class);
+        System.out.println(parroterinioBean.getName());
+
+        /*
+        * Getting basic data types from context:
+        * */
+        String caption = context.getBean("itsOverCaption", String.class);
+        System.out.println(caption);
+
+        Integer number = context.getBean("nineK", Integer.class);
+        System.out.println(number);
+
     }
 }
